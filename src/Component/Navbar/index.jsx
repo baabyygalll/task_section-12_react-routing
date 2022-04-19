fc9c27de999ec624500435d1bd185f5ea947d35c
baabyygalll <andiraazzahra19@gmail.com>
2022-04-19 15:16:36 +0700

    add sidebar

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+// css
+import './style.module.css';
+
+
 
 const Navbar = () => {
 
-    return(
-        <div>
-            <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            </nav>
-        </div>
-    )
+    return (
+      <div>
+      
+        <sidebar className="sidebar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </sidebar>
+      </div>
+    );
 }
 export default Navbar;
--- src/Component/TodoList/index.jsx
+++ src/Component/TodoList/index.jsx
@@ -8,7 +8,8 @@ const TodoList = ({dataTodos}) => {
        <div className="list">
            <ul>
               { dataTodos.map((todo) => 
-                   { return <li key={todo.id}>{todo.title} <button>Hapus</button> </li>  })
+                   { return <li key={todo.id}>{todo.title} <button>Hapus</button> </li>  }
+                   )
               }
              </ul>
        </div>
